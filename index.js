884e2ddfd6eac527b2104006d3fbd732dbfcf9c0
SimpleFixer <100975456+SimpleFixer@users.noreply.github.com>
2022-03-06 11:01:05 +0100

    fix

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const driverData = fetch('./Drivers.json')
                   .then(response => response.json())
                   .then(data => {
-                    console.log('success', response.json());
-                    renderMainMenu(response.json());
-                    return response.json();
+                    console.log('success', data.drivers);
+                    renderMainMenu(data.drivers);
+                    return data;
                   })
                   .catch(function (err){
                     console.warn('Something went wrong', err)
