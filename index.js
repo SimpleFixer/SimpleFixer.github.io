df33a63acf0c4036c1abd56696013f06480d08cc
SimpleFixer <100975456+SimpleFixer@users.noreply.github.com>
2022-03-06 11:07:31 +0100

    no clue

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const driverData = fetch('./Drivers.json')
                   .then(response => response.json())
                   .then(data => {
-                    console.log('success', data.drivers);
-                    renderMainMenu(data.drivers);
-                    return data;
+                    console.log('success', data.object.drivers);
+                    renderMainMenu(data.object.drivers);
+                    return data.object;
                   })
                   .catch(function (err){
                     console.warn('Something went wrong', err)
                   });
 
+const mainMenu = document.getElementById("main-menu");
 
 const renderMainMenu = (drivers) => {
     
     console.log(drivers);
     
     drivers.forEach(driver => {
-        mainMenu.innerHTML += <p>{driver.firstName}</p>
+        mainMenu.innerHTML += `<p>${driver.firstName}</p>`;
     });
 };
 
