9a7205487f0e2425fd2adaeaea64496e1a90e41c
SimpleFixer <100975456+SimpleFixer@users.noreply.github.com>
2022-03-06 10:53:22 +0100

    break

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
 const driverData = fetch('./Drivers.json')
                   .then(function (response){
                     console.log('success', response.json());
-                    value(response);
-                    return response;
+                    renderMainMenu(response.json());
+                    return response.json();
                   })
                   .catch(function (err){
                     console.warn('Something went wrong', err)
                   });
 
-function value(data){
-  return JSON.parse(data);
-};
 
 const renderMainMenu = (drivers) => {
     
     console.log(drivers);
-    /*
+    
     drivers.forEach(driver => {
-        // mainMenu.innerHTML += <p>{driver.firstName}</p>
+        mainMenu.innerHTML += <p>{driver.firstName}</p>
     });
-    console.log(driverData);*/
 };
 
 // renderMainMenu(getDriverData("file:///C:/Users/Theodor/Documents/GitHub/SimpleFixer.github.io/Drivers.json"));
